b3b0d23c4262aa1882d508c4d30e39de20f0f5bf
Edevaldo Batista <edevaldobatistadesouza@gmail.com>
2021-08-01 14:36:23 -0300

    Init migrations

---
+++ src/database/migrations/1627839320051-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1627839320051 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
